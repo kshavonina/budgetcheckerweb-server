64dd310547ac6cfe8d6639d17ba445dea564e386
Kseniia Shavonina <kshavonina@yandex.ru>
2019-02-02 21:20:53 +0300

    Improve User model

--- src/main/java/com/budgetcheckerweb/model/User.java
+++ src/main/java/com/budgetcheckerweb/model/User.java
@@ -1,13 +1,14 @@
 package com.budgetcheckerweb.model;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class User {
 
-    private String name;
-    private String surname;
     private String email;
     private String password;
+    private String name;
+    private String surname;
     private BigDecimal moneyAmount;
 
     public User() {
@@ -15,31 +16,15 @@ public class User {
     }
 
     public User(String email, String password) {
-        this("", "", email, password, BigDecimal.ZERO);
+        this(email, password, "", "", BigDecimal.ZERO);
     }
 
-    public User(String name, String surname, String email, String password, BigDecimal moneyAmount) {
-        this.name = name;
-        this.surname = surname;
+    public User(String email, String password, String name, String surname, BigDecimal moneyAmount) {
         this.email = email;
         this.password = password;
-        this.moneyAmount = moneyAmount;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
         this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
         this.surname = surname;
+        this.moneyAmount = moneyAmount;
     }
 
     public String getEmail() {
@@ -56,6 +41,22 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
     }
 
     public BigDecimal getMoneyAmount() {
@@ -75,5 +76,19 @@ public class User {
                 ", password='" + password + '\'' +
                 ", moneyAmount=" + moneyAmount +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(email, user.email) &&
+                Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, password);
     }
 }
