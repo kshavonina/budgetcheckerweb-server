1e1f80c2269bc298e7f86b16b9c30ff715ca2681
Kseniia Shavonina <kshavonina@yandex.ru>
2019-02-07 17:24:19 +0300

    Add logging, and first attempt of setting up Spring Security OAuth2 support (process of receiving token with credentials equals for user and client entities is workable)

--- src/main/java/com/budgetcheckerweb/config/WebAppInitializer.java
+++ src/main/java/com/budgetcheckerweb/config/WebAppInitializer.java
@@ -1,28 +1,32 @@
 package com.budgetcheckerweb.config;
 
-import com.budgetcheckerweb.config.security.WebSecurityConfig;
+import com.budgetcheckerweb.config.security.CORSFilter;
 import com.budgetcheckerweb.config.swagger.SwaggerConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
 
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class<?>[]{
-                WebConfig.class,
-                SwaggerConfig.class,
-                WebSecurityConfig.class
-        };
-    }
-
-    @Override
-    protected Class<?>[] getServletConfigClasses() {
         return new Class<?>[]{
                 WebConfig.class
         };
     }
 
     @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        Filter [] singleton = { new CORSFilter()};
+        return singleton;
     }
 }
